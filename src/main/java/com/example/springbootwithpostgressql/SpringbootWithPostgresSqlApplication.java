cc37f69875c8d49a29a1d092523a4e11620f8b95
phNamBlueBerryYumYum <namph@edsolabs.com>
2023-03-27 15:41:53 +0700

    first commit

---
+++ src/main/java/com/example/springbootwithpostgressql/SpringbootWithPostgresSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithpostgressql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithPostgresSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithPostgresSqlApplication.class, args);
+    }
+
+}
